c287456b48c298357bb57198fccc1b2c7fbd3e24
Ивакин Никита <nikita.ivakin@nure.ua>
2024-05-21 23:43:22 +0300

    added mappings to the note controller

--- src/main/java/com/example/applicationformanagmentnotes/controller/NoteController.java
+++ src/main/java/com/example/applicationformanagmentnotes/controller/NoteController.java
@@ -1,12 +1,42 @@
 package com.example.applicationformanagmentnotes.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.applicationformanagmentnotes.entity.Note;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class NoteController {
 
+    @GetMapping("/notes")
+    public ResponseEntity<List<Note>> getAllNotes(){
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
+    }
 
+    @GetMapping("/notes/{id}")
+    public ResponseEntity<Note> getSpecifiedNote(@PathVariable Integer id){
+        return new ResponseEntity<>(new Note(), HttpStatus.OK);
+    }
 
+    @PostMapping("/notes")
+    public ResponseEntity<Boolean> createNote(@RequestBody Note note){
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
 
+    @PutMapping("/notes/{id}")
+    public ResponseEntity<Boolean> updateNote(@PathVariable Integer id){
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/notes/{id}")
+    public ResponseEntity<Boolean> deleteNote(@PathVariable Integer id){
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
 }
