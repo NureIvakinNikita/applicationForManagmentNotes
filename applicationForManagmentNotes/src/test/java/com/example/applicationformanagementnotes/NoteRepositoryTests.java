8f8e4ba6cced4d6bcf79d84ab7b3a428c58ab29f
Ивакин Никита <nikita.ivakin@nure.ua>
2024-06-04 09:39:42 +0300

    added profile for tests

--- applicationForManagmentNotes/src/test/java/com/example/applicationformanagementnotes/NoteRepositoryTests.java
+++ applicationForManagmentNotes/src/test/java/com/example/applicationformanagementnotes/NoteRepositoryTests.java
@@ -6,11 +6,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class NoteRepositoryTests {
 
--- applicationForManagmentNotes/src/test/java/com/example/applicationformanagementnotes/NoteServiceImplTests.java
+++ applicationForManagmentNotes/src/test/java/com/example/applicationformanagementnotes/NoteServiceImplTests.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.util.Collections;
@@ -24,6 +25,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 public class NoteServiceImplTests {
 
     @Mock
